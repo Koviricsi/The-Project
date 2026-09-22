9d1e1e28496d8e66c46d910749e3897f7666be11
Koviricsi <richard.kovi.07@gmail.com>
2026-09-22 07:58:32 +0200

    prettier

--- js/nav.js
+++ js/nav.js
@@ -2,11 +2,11 @@ let nav = document.getElementsByTagName("nav")[0];
 let navBtn = document.getElementById("nav-hamburger");
 
 navBtn.addEventListener("click", () => {
-    nav.classList.toggle("open");
-})
+  nav.classList.toggle("open");
+});
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth > 1075){
-        nav.classList.remove("open");
-    }
-})
+  if (window.innerWidth > 1075) {
+    nav.classList.remove("open");
+  }
+});
